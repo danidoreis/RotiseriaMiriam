c5a350d9e4f3e8a739bb0f57c6c656f4fb02d471
Dani Do Reis <61333888+danidoreis@users.noreply.github.com>
2020-09-14 01:12:15 -0300

    Create tablas.js

---
+++ js/tablas.js
@@ -0,0 +1,119 @@
+
+$(document).ready(function(){
+    $("#botonocultamuestra").click(function(){
+       $("#divocultamuestra").each(function() {
+         displaying = $(this).css("display");
+         if(displaying == "block") {
+           $(this).fadeOut('slow',function() {
+            $(this).css("display","none");
+           });
+         } else {
+           $(this).fadeIn('slow',function() {
+             $(this).css("display","block");
+           });
+         }
+       });
+     });
+   });
+
+   $(document).ready(function(){
+    $("#boton").click(function(){
+       $("#divoculta").each(function() {
+         displaying = $(this).css("display");
+         if(displaying == "block") {
+           $(this).fadeOut('slow',function() {
+            $(this).css("display","none");
+           });
+         } else {
+           $(this).fadeIn('slow',function() {
+             $(this).css("display","block");
+           });
+         }
+       });
+     });
+   });
+
+   $(document).ready(function(){
+    $("#botonburger").click(function(){
+       $("#div").each(function() {
+         displaying = $(this).css("display");
+         if(displaying == "block") {
+           $(this).fadeOut('slow',function() {
+            $(this).css("display","none");
+           });
+         } else {
+           $(this).fadeIn('slow',function() {
+             $(this).css("display","block");
+           });
+         }
+       });
+     });
+   });
+
+   $(document).ready(function(){
+    $("#botonpapas").click(function(){
+       $("#divpapas").each(function() {
+         displaying = $(this).css("display");
+         if(displaying == "block") {
+           $(this).fadeOut('slow',function() {
+            $(this).css("display","none");
+           });
+         } else {
+           $(this).fadeIn('slow',function() {
+             $(this).css("display","block");
+           });
+         }
+       });
+     });
+   });
+
+   $(document).ready(function(){
+    $("#botonpastas").click(function(){
+       $("#divpastas").each(function() {
+         displaying = $(this).css("display");
+         if(displaying == "block") {
+           $(this).fadeOut('slow',function() {
+            $(this).css("display","none");
+           });
+         } else {
+           $(this).fadeIn('slow',function() {
+             $(this).css("display","block");
+           });
+         }
+       });
+     });
+   });
+
+   $(document).ready(function(){
+    $("#botonmilas").click(function(){
+       $("#divmilas").each(function() {
+         displaying = $(this).css("display");
+         if(displaying == "block") {
+           $(this).fadeOut('slow',function() {
+            $(this).css("display","none");
+           });
+         } else {
+           $(this).fadeIn('slow',function() {
+             $(this).css("display","block");
+           });
+         }
+       });
+     });
+   });
+
+   $(document).ready(function(){
+    $("#botonn").click(function(){
+       $("#divv").each(function() {
+         displaying = $(this).css("display");
+         if(displaying == "block") {
+           $(this).fadeOut('slow',function() {
+            $(this).css("display","none");
+           });
+         } else {
+           $(this).fadeIn('slow',function() {
+             $(this).css("display","block");
+           });
+         }
+       });
+     });
+   });
