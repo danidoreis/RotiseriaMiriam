2e352387536afa9193d8608d3f2b1724f4c5ee0b
Dani Do Reis <61333888+danidoreis@users.noreply.github.com>
2020-09-14 01:33:36 -0300

    Create ajax.js

---
+++ carrito/libs/ajax.js
@@ -0,0 +1,103 @@
+$(function(){
+	var ENV_WEBROOT = "../";
+	
+	$(".btn-agregar-producto").off("click");
+	$(".btn-agregar-producto").on("click", function(e) {
+		var cantidad = $("#txt_cantidad").val();
+		var producto_id = $("#cbo_producto").val();
+		if(producto_id!=0){
+			if(cantidad!=''){
+				$.ajax({
+					url: 'Controller/ProductoController.php?page=1',
+					type: 'post',
+					data: {'producto_id':producto_id, 'cantidad':cantidad},
+					dataType: 'json',
+					success: function(data) {
+						if(data.success==true){
+							$("#txt_cantidad").val('');
+							alertify.success(data.msj);
+							$(".detalle-producto").load('detalle.php');
+						}else{
+							alertify.error(data.msj);
+						}
+					},
+					error: function(jqXHR, textStatus, error) {
+						alertify.error(error);
+					}
+				});				
+			}else{
+				alertify.error('Ingrese una cantidad');
+			}
+		}else{
+			alertify.error('Seleccione un producto');
+		}
+	});
+	
+	$(".eliminar-producto").off("click");
+	$(".eliminar-producto").on("click", function(e) {
+		var id = $(this).attr("id");
+		$.ajax({
+			url: 'Controller/ProductoController.php?page=2',
+			type: 'post',
+			data: {'id':id},
+			dataType: 'json'
+		}).done(function(data){
+			if(data.success==true){
+				alertify.success(data.msj);
+				$(".detalle-producto").load('detalle.php');
+			}else{
+				alertify.error(data.msj);
+			}
+		})
+	});
+	
+	$(".guardar-carrito").off("click");
+	$(".guardar-carrito").on("click", function(e) {
+		$.ajax({
+			url: 'Controller/ProductoController.php?page=3',
+			type: 'post',
+			dataType: 'json',
+			success: function(data) {
+				if(data.success==true){
+					$("#txt_cantidad").val('');
+					alertify.success(data.msj);
+					//$(".detalle-producto").load('detalle.php');
+					setTimeout(function(){
+					  window.location.href = 'impresion.php?id='+data.idventa;
+					}, 3000);
+				}else{
+					alertify.error(data.msj);
+				}
+			},
+			error: function(jqXHR, textStatus, error) {
+				alertify.error(error);
+			}
+		});				
+	});
+	
+	$(".guardar-enviar-carrito").off("click");
+	$(".guardar-enviar-carrito").on("click", function(e) {
+		$.ajax({
+			url: 'Controller/ProductoController.php?page=4',
+			type: 'post',
+			dataType: 'json',
+			success: function(data) {
+				if(data.success==true){
+					$("#txt_cantidad").val('');
+					alertify.success(data.msj);
+					//alertify.success(data.success_email);
+					$(".detalle-producto").load('detalle.php');
+					/*setTimeout(function(){
+					  window.location.href = 'impresion.php?id='+data.idventa;
+					}, 3000);*/
+				}else{
+					alertify.error(data.msj);
+				}
+			},
+			error: function(jqXHR, textStatus, error) {
+				alertify.error(error);
+			}
+		});				
+	});
+	
+});
